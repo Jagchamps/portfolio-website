2d32bc1e5fffc86b75245a0ef3af65a87072e6e3
Jonathan Champion <jonathan.ag.champion@gmail.com>
2020-02-03 01:31:01 +0000

    Updates

---
+++ precache-manifest.bd513bef98d68cdbd80a383f97cbcfc2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b7ad9fba3d5df03f0cfe38093f40ed",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "9db1cc63b013df35758b",
+    "url": "/portfolio-website/static/css/main.fdada002.chunk.css"
+  },
+  {
+    "revision": "00fd376b2a64e2f0fb15",
+    "url": "/portfolio-website/static/js/2.9add1505.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio-website/static/js/2.9add1505.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9db1cc63b013df35758b",
+    "url": "/portfolio-website/static/js/main.06c37ae0.chunk.js"
+  },
+  {
+    "revision": "71482691d97cc15591cd",
+    "url": "/portfolio-website/static/js/runtime-main.f6517661.js"
+  },
+  {
+    "revision": "7830c07fe20a7ce814551470dec55b55",
+    "url": "/portfolio-website/static/media/dogface.7830c07f.jpg"
+  },
+  {
+    "revision": "bd5f876615dce0fcb71278642d00231e",
+    "url": "/portfolio-website/static/media/nujazz-logo.bd5f8766.png"
+  },
+  {
+    "revision": "07a6cb67409bdc33aace065c487dc43d",
+    "url": "/portfolio-website/static/media/profile.07a6cb67.jpg"
+  }
+]);
