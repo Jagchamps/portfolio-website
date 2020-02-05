bc8d4e12f73b9063f64324a5e0d9961d6c8f200d
Jonathan Champion <jonathan.ag.champion@gmail.com>
2020-02-05 19:18:29 +0000

    Trying to build github pages

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
     NavLink
   } from 'react-router-dom';
 import {slide as Menu} from 'react-burger-menu';
-import AboutMe from './AboutMe.js';
-import Portfolio from './Portfolio.js';
-import OtherProjects from './OtherProjects.js';
-import Home from './Home.js';
 import '../styles/App.scss';
 
 var styles = {
@@ -28,7 +21,7 @@ var styles = {
 function BurgerMenu() {
     return (
         <Menu right width={ "100%" } className="bm-menu-container">
-            <NavLink style={styles.link} activeStyle={styles.active} exact to="/">Home</NavLink>
+            <NavLink style={styles.link} activeStyle={styles.active} exact to="/portfolio-website">Home</NavLink>
             <NavLink style={styles.link} activeStyle={styles.active} to="/aboutme">About me</NavLink>
             <NavLink style={styles.link} activeStyle={styles.active} to="/portfolio">Portfolio</NavLink>
             <NavLink style={styles.link} activeStyle={styles.active} to="/otherprojects">Other Projects</NavLink>
