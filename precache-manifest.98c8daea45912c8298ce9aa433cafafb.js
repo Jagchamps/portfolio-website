83b19942b155b52ab8e53f813e8e5039573e1c2b
Jonathan Champion <jonathan.ag.champion@gmail.com>
2020-09-05 11:05:13 +0100

    Updates

---
+++ precache-manifest.98c8daea45912c8298ce9aa433cafafb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b9ac3d56169878f4cbab4c1cf504b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f383dc9788b3cd3a12e",
+    "url": "/static/css/main.af5a2616.chunk.css"
+  },
+  {
+    "revision": "fdd3e43d2874dfe15c2c",
+    "url": "/static/js/2.b555103a.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.b555103a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f383dc9788b3cd3a12e",
+    "url": "/static/js/main.cebad429.chunk.js"
+  },
+  {
+    "revision": "35ec47dfee4c65c275be",
+    "url": "/static/js/runtime-main.c6e37e88.js"
+  },
+  {
+    "revision": "07a6cb67409bdc33aace065c487dc43d",
+    "url": "/static/media/profile.07a6cb67.jpg"
+  }
+]);
